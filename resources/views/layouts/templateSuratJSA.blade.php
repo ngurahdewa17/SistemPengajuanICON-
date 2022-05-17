1ea5b9f4cc1237c779e2783a4e066f017a8b8e4e
ngurahdewa17 <ngurahdewa17@gmail.com>
2022-05-17 15:29:22 +0800

    Update_SistemPengajuan_V2

---
+++ resources/views/layouts/templateSuratJSA.blade.php
@@ -0,0 +1,127 @@
+<!Doctype html>
+<html>
+    <title>KOP SURAT</title>
+    <style type="text/css">
+        body{font-family: arial; background-color: #ccc}
+        .rangkasurat{width: 980px;margin:0 auto; background-color:#fff; height: 1600px; padding: 20px;}
+        table{border-bottom: 5px solid #000; padding:2px}
+        .tengah{text-align: center;line-height: 5px;}
+    </style>
+    <link rel="stylesheet" href="{{asset('template.css')}}">
+    <body>
+        <div class="rangkasurat">
+            <table width="100%">
+                <tr>
+                    {{-- <td><img src={{asset('icon+.png')}} width="140px"></td> --}}
+                    <td><img src="{{ asset('storage/template/iconplus.jpg') }}" width="150px"  class="img img-responsive"/></td>
+                    <td class="tengah">
+                        <h1>FORM PERMOHONAN PEKERJAAN</h1><br>
+                        <h2>SBU BALI ICON PLUS</h2>
+                        <h1>DENPASAR</h1>
+                        <b>Jl. Gunung Mandala Wangi No. 15, Kel Tegal Harum Denpasar, Bali</b><br>
+                       
+                    </td>
+                    <td><img src="{{ asset('storage/template/pln.jpg') }}" width="150px"  class="img img-responsive"/></td>
+                </tr>
+            </table> 
+            <br><br><br>
+            <table align="center">
+                <tr>
+                    <td>Nama Pemohon</td>
+                    <td width="500"> :</td>
+                </tr>
+                <tr>
+                    <td>Nama Perusahaan</td>
+                    <td width="500"> :</td>
+                </tr>
+                <tr>
+                    <td>Judul Pekerjaan</td>
+                    <td width="500"> :</td>
+                </tr>
+                <tr>
+                    <td>No.SP2KPA</td>
+                    <td width="500"> :</td>
+                </tr>
+                <tr>
+                    <td>Hari Tanggal Mulai Pekerjaan</td>
+                    <td width="500"> :</td>
+                </tr>
+                <tr>
+                    <td>Hari Tanggal Selesai Pekerjaan</td>
+                    <td width="500"> :</td>
+                </tr>
+                <tr>
+                    <td>Nama Lokasi Pekerjaan</td>
+                    <td width="500"> :</td>
+                </tr>
+                <tr>
+                    <td>Aktifitas Pekerjaan</td>
+                    <td width="500"> :</td>
+                </tr>
+                <tr>
+                    <td>Lokasi</td>
+                    <td width="500"> :</td>
+                </tr>
+                <tr>
+                    <td>Task List</td>
+                    <td width="500"> :</td>
+                </tr>
+                
+                <tr>
+                    <td colspan="2">Dengan ini Disampaikan bahwa pada hari senin akan
+                        dilaksanakan upacara di halaman<br> kantor diskominfo kukar 
+                        demikian pemberitahuan ini sekian terimakasih.
+                    </td>
+                </tr>
+            </table>
+            <br>
+            
+            <table class="ttd1">
+                <tr>
+                    <td><center>Ketua Diskominfo</center></td>
+                </tr>
+                <tr>
+                    <td height="70">ttd</td>
+                </tr>
+                <tr>
+                    <td><center> <u> Ir. Bahteramsyah </u></center></td>
+                </tr>
+                <tr>
+                    <td>Nim. 192 977827388 001 1</td>
+                </tr>
+            </table>
+            <table class="ttd2">
+                <tr>
+                    <td><center>Ketua Diskominfo</center></td>
+                </tr>
+                <tr>
+                    <td height="70">ttd</td>
+                </tr>
+                <tr>
+                    <td><center> <u> Ir. Bahteramsyah </u></center></td>
+                </tr>
+                <tr>
+                    <td>Nim. 192 977827388 001 1</td>
+                </tr>
+            </table>
+            <table class="ttd3">
+                <tr>
+                    <td><center>Ketua Diskominfo</center></td>
+                </tr>
+                <tr>
+                    <td height="70">ttd</td>
+                </tr>
+                <tr>
+                    <td><center> <u> Ir. Bahteramsyah </u></center></td>
+                </tr>
+                <tr>
+                    <td>Nim. 192 977827388 001 1</td>
+                </tr>
+            </table>
+            
+           
+        </div>
+        <br>
+        
+    </body>
+</html>
